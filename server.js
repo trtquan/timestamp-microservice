e174b5a6d2afd5b3a40772205b053d61caad7703
quantran <trtquan@gmail.com>
2020-09-20 10:23:55 +0700

    get and check valid date and

--- server.js
+++ server.js
@@ -4,7 +4,9 @@
 // init project
 var express = require('express');
 var app = express();
-
+const checkValidDate = (date) => {
+  return d instanceof Date && !isNaN(d);
+}
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -26,7 +28,16 @@ app.get("/api/hello", function (req, res) {
 
 app.get("api/timestamp/:date_string", (req,res) => {
   const {date_string} = req.params;
-  res.json({"error" : "Invalid Date" })
+  const date = new Date(date_string)
+  const isValidDate = checkValidDate(date);
+  if(isValidDate) {
+    res.json({
+      "unix": date.getTime(),
+      "utc": date.toUTCString()
+    })
+  } else {
+    res.json({"error" : "Invalid Date" })
+  }
 })
 
 // listen for requests :)
