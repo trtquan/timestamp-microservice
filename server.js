e22965a9d4834124d4e1144f997620d9316bcf09
quantran <trtquan@gmail.com>
2020-09-20 10:28:42 +0700

    missing slash

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.get("api/timestamp/:date_string", (req,res) => {
+app.get("/api/timestamp/:date_string", (req,res) => {
   const {date_string} = req.params;
   const date = new Date(date_string)
   console.log(date);
